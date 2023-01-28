00a9ff4342c263eb2487bd2b8f2bdfc71ca44c55
Natnael Asmare Mebrate <m.t3sla@gmail.com>
2023-01-28 12:57:20 +0300

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apid6c19751": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "CustomerHandler": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
